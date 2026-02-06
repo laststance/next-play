0e56b9d134a1e8182cdbbc8d2da6e8c0dab3dd64
Ryota Murakami <dojce1048@gmail.com>
2026-02-06 21:01:33 +0900

    chore: update dependencies in package.json and pnpm-lock.yaml, and simplify .prettierrc by removing redundant plugin

--- components/guestbook.tsx
+++ components/guestbook.tsx
@@ -2,12 +2,20 @@
 
 export const Guestbook = () => {
   return (
-    <section className="w-full h-full border border-boder rounded-b-lg">
-      <h2 className="text-2xl">Guestbook</h2>
-      <form>
-        <label htmlFor="name">name</label>
-        <input name="name" type="text" />
-        <textarea name="message"></textarea>
+    <section className="border-boder flex w-full flex-1 flex-col items-center rounded-b-lg border">
+      <div className="grid w-full place-content-center">
+        <h2 className="p-8 text-2xl">Guestbook</h2>
+      </div>
+
+      <form className="flex w-full flex-col items-center gap-4">
+        <div className="flex flex-1 items-center gap-4">
+          <label htmlFor="guest">guest</label>
+          <input name="guest" type="text" className="border-border border" />
+        </div>
+        <div className="flex flex-1 items-center gap-4">
+          <label htmlFor="message">message</label>
+          <textarea name="message" className="border-border border"></textarea>
+        </div>
       </form>
     </section>
   )
