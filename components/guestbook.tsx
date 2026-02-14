1cca7c6476e1c7a6fd36442c9185d3e383181eee
Ryota Murakami <dojce1048@gmail.com>
2026-02-14 14:57:24 +0900

    fix: correct indentation in Guestbook component form closing tag for improved readability

--- components/guestbook.tsx
+++ components/guestbook.tsx
@@ -7,15 +7,18 @@ export const Guestbook = () => {
         <h2 className="p-8 text-2xl">Guestbook</h2>
       </div>
 
-      <form className="flex w-full flex-col items-center gap-4">
-        <div className="flex flex-1 items-center gap-4">
-          <label htmlFor="guest">guest</label>
-          <input name="guest" type="text" className="border-border border" />
-        </div>
-        <div className="flex flex-1 items-center gap-4">
-          <label htmlFor="message">message</label>
-          <textarea name="message" className="border-border border"></textarea>
-        </div>
+      <form className="grid w-[80%] grid-cols-[auto_1fr] items-center gap-4 px-8">
+        <label htmlFor="guest">guest</label>
+        <input
+          name="guest"
+          type="text"
+          className="border-border rounded-md border px-3 py-2"
+        />
+        <label htmlFor="message">message</label>
+        <textarea
+          name="message"
+          className="border-border rounded-md border px-3 py-2"
+        ></textarea>
       </form>
     </section>
   )
