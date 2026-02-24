dcb6f8f40b446fe01a8cddb12dcc3bc566e8840a
Ryota Murakami <dojce1048@gmail.com>
2026-02-24 22:11:33 +0900

    feat: create guestbook page to enhance user interaction and engagement

---
+++ app/guestbook/page.tsx
@@ -0,0 +1,14 @@
+import prisma from '@/lib/prisma'
+import { Guestbook } from '@/components/guestbook'
+
+export default async function Page() {
+  const notes = await prisma.guestNote.findMany({
+    orderBy: { createdAt: 'desc' },
+  })
+
+  return (
+    <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between px-16 py-32">
+      <Guestbook initialNotes={notes} />
+    </main>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import prisma from '@/lib/prisma'
-import { Guestbook } from '@/components/guestbook'
+import Link from 'next/link'
 
 export default async function Home() {
-  const notes = await prisma.guestNote.findMany({
-    orderBy: { createdAt: 'desc' },
-  })
   return (
     <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between px-16 py-32">
-      <Guestbook initialNotes={notes} />
+      <Link href="/guestbook">guestbook</Link>
     </main>
   )
 }
