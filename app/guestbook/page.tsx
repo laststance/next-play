0a9e22a37caed1ccd19f05eee04f9d0387bc80de
Ryota Murakami <dojce1048@gmail.com>
2026-02-25 20:52:29 +0900

    feat: add navigation link to home and adjust layout in guestbook page for improved user experience

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -1,4 +1,6 @@
 import prisma from '@/lib/prisma'
+import Link from 'next/link'
+import { ArrowLeftIcon } from 'lucide-react'
 import { Guestbook } from '@/components/guestbook'
 
 export default async function Page() {
@@ -7,7 +9,12 @@ export default async function Page() {
   })
 
   return (
-    <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between px-16 py-32">
+    <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between px-16 py-8">
+      <div className="w-full flex-1">
+        <Link className="inline-flex place-items-center gap-2" href="/">
+          <ArrowLeftIcon className="h-4 w-4" /> Home
+        </Link>
+      </div>
       <Guestbook initialNotes={notes} />
     </main>
   )
