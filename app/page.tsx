9b6d3ac865a426dded7d0bcef6e8be695f06f6f5
Ryota Murakami <dojce1048@gmail.com>
2026-03-03 23:25:33 +0900

    feat: enhance UI styling and add buttons for improved navigation
    This commit introduces a new button component and updates the global CSS to enhance the applications visual design. The addition of buttons improves user experience by providing a clearer and more interactive way to navigate to the guestbook and tab pages. Additionally, adjustments to color variables ensure a consistent aesthetic across light and dark themes, enhancing accessibility and visual appeal.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import { Main } from '@/components/main'
 import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 
 export default async function Home() {
   return (
     <Main>
-      <div className="flex-1">
-        <Link href="/guestbook">guestbook</Link>
-        <Link href="/tab">tab</Link>
+      <div className="flex-1 space-x-4">
+        <Button asChild variant="outline">
+          <Link href="/guestbook">guestbook</Link>
+        </Button>
+        <Button asChild variant="outline">
+          <Link href="/tab">tab</Link>
+        </Button>
       </div>
     </Main>
   )
